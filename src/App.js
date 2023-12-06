d3742caaa04b9e248da29e17aee7439fd706368e
Vihan Gayathra <s92068813@ousl.lk>
2023-12-07 01:21:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import './App.css';
-import { AuthProvider } from './contexts/AuthContext';
-import SignUp from './pages/SignUp';
 import Resume from "./pages/Resume";
-import SignIn from "./pages/SignIn";
-import PrivateRoute from "./PrivateRoute";
-import Dashboard from "./pages/Dashboard";
+
 
 function App() {
   return (
     <Router>
-      <AuthProvider>
         <Routes>
           <Route path="/" Component={Resume} />
-          <Route path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>}/>
-          <Route path="/signup" Component={SignUp} />
-          <Route path="/signin" Component={SignIn} />
         </Routes>
-      </AuthProvider>
     </Router>
   );
 }
--- src/pages/components/Education.js
+++ src/pages/components/Education.js
@@ -1,34 +1,56 @@
 import React from 'react'
-import { CardHeader, Container,Card, Accordion, AccordionItem } from 'react-bootstrap'
+import { Container,Card, Accordion, AccordionItem, ListGroup } from 'react-bootstrap'
 
-function Education() {
+
+function Education() { 
+
+
   return (
     <>
-    <hr/>
-    <h1 className='text-center'>Education</h1>
-    <Container className='h-100 d-flex' >
-        <Container className='d-flex align-items-center justify-content-center'>
-            <Card style={{maxWidth:'18rem'}}>
-                <Accordion className='m-3'>
-                    <AccordionItem eventKey='0'>
+        <hr/>
+        <h1 className='text-center'>Education</h1>
+        <Container className='h-100 d-flex' >
+            <Container className='d-flex align-items-center justify-content-center'>
+                <Card style={{maxWidth:'18rem'}}>
+                    <Accordion className='m-3'>
+                        <AccordionItem eventKey='0'>
+                            <Accordion.Header>
+                                Advanced Level
+                            </Accordion.Header>
+                            <Accordion.Body>
+                                I have completed my advanced level in Science Stream in,
+                                <br/>
+                                <ListGroup as="ul">
+                                    <ListGroup.Item>Biology</ListGroup.Item>
+                                    <ListGroup.Item>Chemistry</ListGroup.Item>
+                                    <ListGroup.Item>Physics</ListGroup.Item>
+                                </ListGroup>
+                                <span>Richmond College Galle</span>
+                            </Accordion.Body>
+                        </AccordionItem>
+                    </Accordion>
+                </Card>
+            </Container>
+            <Container className='d-flex align-items-center justify-content-center'>
+                <Card>
+                    <Accordion className='m-3'>
                         <Accordion.Header>
-                            Advanced Level
+                            Bachelor Of Software Engineering
                         </Accordion.Header>
                         <Accordion.Body>
-                            I have completed my advanced level in Science Stream in,
-                            <br/>
-                            <span>Richmond College Galle</span>
+                            <ListGroup as='ul'>
+                               <ListGroup.Item>Communication Skills</ListGroup.Item>
+                               <ListGroup.Item>Economics and Marketing for Engineering</ListGroup.Item>
+                               <ListGroup.Item>Computer Security Concepts</ListGroup.Item>
+                               <ListGroup.Item>Introduction to object oriented programming</ListGroup.Item>
+                               <ListGroup.Item>Information systems and data management</ListGroup.Item>
+                               <ListGroup.Item>Introduction to Mobile Application Development</ListGroup.Item>
+                            </ListGroup>
                         </Accordion.Body>
-                    </AccordionItem>
-                </Accordion>
-            </Card>
+                    </Accordion>
+                </Card>
+            </Container>
         </Container>
-        <Container className='d-flex align-items-center justify-content-center'>
-            <Card>
-                <CardHeader>BSE</CardHeader>
-            </Card>
-        </Container>
-    </Container>
     </>
   )
 }
